55dc9b5acb7fcd616c1c2c7f40cfdbac24fa6d1f
Paul Kronabethleitner <kronabethleitner@gmail.com>
2017-02-14 09:38:58 +0000

    Last changes before Live Mailer and more

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,9 +5,15 @@ class ContactsController < ApplicationController
    def create
       @contact = Contact.new(contact_params)
       if @contact.save
-         redirect_to new_contact_path, notice: "Message sent."
+         name = params[:contact][:name]
+         email = params[:contact][:email]
+         body = params[:contact][:comments]
+         ContactMailer.contact_email(name,email,body).deliver
+         flash[:success] = "Message sent."
+         redirect_to new_contact_path
       else
-         redirect_to new_contact_path, notice: "Error occurred."
+         flash[:danger] = @contact.errors.full_messages.join(", ")
+         redirect_to new_contact_path
       end
    end
    
