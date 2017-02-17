7ead9ce983bb1c59b6eccc0f82c4c98d74e4e6d0
Paul Kronabethleitner <kronabethleitner@gmail.com>
2017-02-17 17:08:58 +0000

    Added profile stuff and paperclip gem

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,25 @@
+class ProfilesController < ApplicationController
+  # GET to /users/:user_id/profile/new
+  def new
+      #Render blank profile details form
+      @profile = Profile.new
+  end
+  
+  # POST to /user/:user_id/profile
+  def create
+      #ensure to have right user
+      @user = User.find( params[:user_id] )
+      #create linked profile for this user
+      @profile = @user.build_profile( profile_params)
+      if @profile.save
+         flash[:success] = "Profile updated!"
+         redirect_to user_path( params[:user_id] )
+     else
+         render action: :new
+     end
+  end
+  private
+      def profile_params
+          params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      end
+end
